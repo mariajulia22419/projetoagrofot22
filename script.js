5c3c789f3d410ee7b718a9413180abc014aa6a26
mariajulia22419 <rodrigues.silva.maria2203@escola.pr.gov.br>
2026-06-10 13:36:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+document.getElementById("explorar").addEventListener("click", () => {
+
+document.querySelector(".destaques")
+.scrollIntoView({
+behavior:"smooth"
+});
+
+});
+
+const counters = document.querySelectorAll(".contador");
+
+counters.forEach(counter => {
+
+const update = () => {
+
+const target = +counter.getAttribute("data-target");
+
+const current = +counter.innerText;
+
+const increment = target / 100;
+
+if(current < target){
+
+counter.innerText = Math.ceil(current + increment);
+
+setTimeout(update,20);
+
+}else{
+
+counter.innerText = target;
+
+}
+
+};
+
+update();
+
+});
